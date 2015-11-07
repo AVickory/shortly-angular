81a644103ba9b76b1e7c880c337ccf14dde2a54c
avickory <andrew.vickory@gmail.com>
2015-11-06 20:51:26 -0800

    Will did it.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -4,8 +4,12 @@ angular.module('shortly.links', [])
   Links.getLinks(function (links) {
     $scope.links = links.data;
   });
+  $scope.linkRedirect = function () {
+    $location.path(this.link.url);
+    console.log(this.link.visits);
+    };
   $scope.goToCreate = function () {
-    $location.url('/create');
+    $location.url;
   };
 });
 
